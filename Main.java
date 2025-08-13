9c9c0d9345e6a500ddcfb59e918583d5c2a6fa87
Anamika Kumari <anamikakumari.71611@gmail.com>
2025-08-13 09:16:08 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Anamika");
+        System.out.println("FB");
+        System.out.println("04");
+    }
+    
+}
