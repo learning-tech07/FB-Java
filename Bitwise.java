bdf4e3855ceba54c2d6c9604043047a744768281
Anamika Kumari <anamikakumari.71611@gmail.com>
2025-08-14 10:28:48 +0530

    Add files via upload

---
+++ Bitwise.java
@@ -0,0 +1,14 @@
+public class Bitwise {
+    public static void main(String[] args) {
+       // int a = 2 , b = 3;
+       // System.out.println(a&b); 
+        int a = 6, b = 5 ;
+        System.out.println(a&b); //output-> 4
+        System.out.println(a|b); //output-> 7
+        System.out.println(a^b); //output-> 3
+
+        
+    
+    }
+    
+}
