665982e6be4a1e264c01ebceca6023d34d03abaa
Anamika Kumari <anamikakumari.71611@gmail.com>
2025-08-25 08:45:38 +0530

    break statement

--- loop.java
+++ loop.java
@@ -1,9 +1,11 @@
 public class loop {
     public static void main(String[] args){
-    //     for(int i=1 ; i<=5 ; i++){
-    //         System.out.println(i);
-    //     }
-    // }
+        for(int i=1 ; i<=5 ; i++){
+            
+            if(i==5)break;
+            System.out.println(i);
+        }
+    }
 
     // int i = 1;
     // while(i<=5){
@@ -11,11 +13,11 @@ public class loop {
     //     i++;
     // }
 
-    int i = 1;
-    do{
-        System.out.println(i);
-        i++;
-    }while(i<=5);
+    // int i = 1;
+    // do{
+    //     System.out.println(i);
+    //     i++;
+    // }while(i<=5);
 
 }
-}
+
