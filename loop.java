64feadd5e89084e7f53136c978c7fa755d7dfc3a
Anamika Kumari <anamikakumari.71611@gmail.com>
2025-08-25 08:37:50 +0530

    Print natural numbers

---
+++ loop.java
@@ -0,0 +1,21 @@
+public class loop {
+    public static void main(String[] args){
+    //     for(int i=1 ; i<=5 ; i++){
+    //         System.out.println(i);
+    //     }
+    // }
+
+    // int i = 1;
+    // while(i<=5){
+    //     System.out.println(i);
+    //     i++;
+    // }
+
+    int i = 1;
+    do{
+        System.out.println(i);
+        i++;
+    }while(i<=5);
+
+}
+}
