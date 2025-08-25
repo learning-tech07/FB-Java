b180ce062d1c76352b40eaf642441b1dd009621b
Anamika Kumari <anamikakumari.71611@gmail.com>
2025-08-25 08:47:31 +0530

    continue statement

--- loop.java
+++ loop.java
@@ -1,23 +1,12 @@
 public class loop {
     public static void main(String[] args){
-        for(int i=1 ; i<=5 ; i++){
+        for(int i=1 ; i<=10 ; i++){
             
-            if(i==5)break;
+            if(i==5)continue;
             System.out.println(i);
         }
     }
 
-    // int i = 1;
-    // while(i<=5){
-    //     System.out.println(i);
-    //     i++;
-    // }
-
-    // int i = 1;
-    // do{
-    //     System.out.println(i);
-    //     i++;
-    // }while(i<=5);
-
+    
 }
 
