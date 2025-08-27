47d83cd6f48cbad34ce2047b686c1b14a8b71f03
Anamika Kumari <anamikakumari.71611@gmail.com>
2025-08-27 08:56:05 +0530

    Explicit Conversion

---
+++ ExplicitConversion.java
@@ -0,0 +1,15 @@
+public class ExplicitConversion {
+    public static void main(String[] args) {
+        double doubleNum = 99.99;
+        int intNum = (int) doubleNum; // Explicit conversion from double to int
+
+        int bigValue = 300;
+        byte smallValue = (byte) bigValue; // Explicit conversion from int to byte
+
+        System.out.println("Double value: " + doubleNum);
+        System.out.println("Int (after casting ): " + intNum);
+        System.out.println("Int 300 to Byte(after casting): " + smallValue);
+    
+    }
+    
+}
