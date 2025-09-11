e05665eadba7cd87ce5e0294b226a8007595cda0
Anamika <anamikakumari.71611@gmail.com>
2025-09-11 09:07:29 +0530

    Assignment1

--- Assignment1/Fibonacci.java
+++ Assignment1/Fibonacci.java
@@ -1,1 +1,19 @@
+package Assignment1;
+import java.util.Scanner;
+public class Fibonacci {
 
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 0 ; i<n ; i++){
+            System.out.print(a + " ");
+            int c = a + b;
+            a = b ;
+            b = c;
+            
+        }
+
+    }
+}
